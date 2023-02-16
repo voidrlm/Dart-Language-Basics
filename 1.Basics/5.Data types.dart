ed38bb28fefce3f010f8e036e6506255940a74ca
voidrlm <varjun609@gmail.com>
2023-02-16 10:25:55 +0530

    Data types

---
+++ 1.Basics/5.Data types.dart
@@ -0,0 +1,20 @@
+// Numbers
+// Strings
+// Booleans
+// Lists
+// Maps
+// Sets
+// Runes
+// Null
+
+
+// Numbers	int, double, num	It represents numeric values
+// Strings	String	It represents a sequence of characters
+// Booleans	bool	It represents Boolean values true and false
+// Lists	List	It is an ordered group of items
+// Maps	Map	It represents a set of values as key-value pairs
+// Sets	Set	It is an unordered list of unique values of same types
+// Runes	runes	It represents Unicode values of String
+// Null	null	It represents null value
+
+
