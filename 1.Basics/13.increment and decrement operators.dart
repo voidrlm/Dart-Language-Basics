60e4e6c73255c183c763f2e63673af70d295d504
voidrlm <varjun609@gmail.com>
2023-02-16 10:27:49 +0530

    Increment deccrment operators

---
+++ 1.Basics/13.increment and decrement operators.dart
@@ -0,0 +1,20 @@
+  //increment and decrement operators
+  void main() {
+// declaring two numbers
+    int num1 = 0;
+    int num2 = 0;
+
+// performing increment / decrement operator
+
+// pre increment
+    num2 = ++num1;
+    print("The value of num2 is $num2");
+
+// reset value to 0
+    num1 = 0;
+    num2 = 0;
+
+// post increment
+    num2 = num1++;
+    print("The value of num2 is $num2");
+  }
