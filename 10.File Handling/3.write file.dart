539130a1895a9e4edb4675a1cc1842ac95b9bebd
voidrlm <varjun609@gmail.com>
2023-02-16 20:22:04 +0530

    Write file

---
+++ 10.File Handling/3.write file.dart
@@ -0,0 +1,10 @@
+// dart program to write to file
+import 'dart:io';
+
+void main() {
+  // open file
+  File file = File('test.txt');
+  // write to file
+  file.writeAsStringSync('Welcome to test.txt file.');
+  print('File written.');
+}
