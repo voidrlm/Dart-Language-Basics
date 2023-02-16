a842e05bd3a8ce67cc31ec372f6c6df31bae78b2
voidrlm <varjun609@gmail.com>
2023-02-16 10:28:02 +0530

    Assignment operators

---
+++ 1.Basics/14.assignment operators.dart
@@ -0,0 +1,11 @@
+void main() {
+  double number = 24;
+  number += 1;
+  print("+= $number");
+  number -= 1;
+  print("-=  $number");
+  number *= 2;
+  print("*= $number");
+  number /= 2;
+  print("/= $number");
+}
