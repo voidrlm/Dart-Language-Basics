dbe2252d451b08229d73d14d425b54098862b6ac
voidrlm <varjun609@gmail.com>
2023-02-16 10:25:17 +0530

    Addition of two numbers

---
+++ 1.Basics/2.Addition of two numbers.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num1 = 10; //declaring number1
+  int num2 = 3; //declaring number2
+
+// Calculation
+  int sum = num1 + num2;
+// displaying the output
+  print("The sum is $sum");
+}
