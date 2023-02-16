6f013489a23a818e636dc21367719fce7363916c
voidrlm <varjun609@gmail.com>
2023-02-16 10:27:34 +0530

    Arithematic operators

---
+++ 1.Basics/12.arithematic operator.dart
@@ -0,0 +1,27 @@
+void main() {
+  //arithematic operators
+  // declaring two numbers
+  int num1 = 10;
+  int num2 = 3;
+
+  // performing arithmetic calculation
+  int sum = num1 + num2; // addition
+  int diff = num1 - num2; // subtraction
+  int unaryMinus = -num1; // unary minus
+  int mul = num1 * num2; // multiplication
+  double div = num1 / num2; // division
+  int div2 = num1 ~/ num2; // integer division
+  int mod = num1 % num2; // show remainder
+
+//Printing info
+  print("The addition is $sum.");
+  print("The subtraction is $diff.");
+  print("The unary minus is $unaryMinus.");
+  print("The multiplication is $mul.");
+  print("The division is $div.");
+  print("The integer division is $div2.");
+  print("The modulus is $mod.");
+//
+//
+//
+}
