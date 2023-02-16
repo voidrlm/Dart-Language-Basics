7c0afdfac945a59c6c8db957a90958fdb4fa1fbd
voidrlm <varjun609@gmail.com>
2023-02-16 10:53:11 +0530

    Function declaration

---
+++ 4.Functions/1.Function declaration.dart
@@ -0,0 +1,12 @@
+// Here num1 and num2 are parameters
+void add(int num1, int num2) {
+  int sum;
+  sum = num1 + num2;
+
+  print("The sum is $sum");
+}
+
+void main() {
+// Here 10 and 20 are arguments
+  add(10, 20);
+}
