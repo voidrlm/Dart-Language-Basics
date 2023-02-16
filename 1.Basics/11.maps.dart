66d81beb207a928878614e9ba04141108d7bb89a
voidrlm <varjun609@gmail.com>
2023-02-16 10:27:20 +0530

    Maps

---
+++ 1.Basics/11.maps.dart
@@ -0,0 +1,10 @@
+// a map is an object where you can store data in key-value pairs.
+void main() {
+  Map<String, String> myDetails = {
+    'name': 'John Doe',
+    'address': 'USA',
+    'fathername': 'Soe Doe'
+  };
+
+  print(myDetails['name']);
+}
