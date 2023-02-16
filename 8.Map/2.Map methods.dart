e386ee3b3dbf265a5a81a2243502f54ec3f7bb9c
voidrlm <varjun609@gmail.com>
2023-02-16 18:08:14 +0530

    Different map methods

---
+++ 8.Map/2.Map methods.dart
@@ -0,0 +1,8 @@
+// keys.toList()	Convert all Maps keys to List.
+// values.toList()	Convert all Maps values to List.
+// containsKey(‘key’)	Return true or false.
+// containsValue(‘value’)	Return true or false.
+// clear()	Removes all elements from the Map.
+// removeWhere()	Removes all elements from the Map if condition is valid.
+
+
