756a24cd512b9e9ab95a9c4d9edd71235259a6ea
voidrlm <varjun609@gmail.com>
2023-02-16 10:26:25 +0530

    Multi LIne String

---
+++ 1.Basics/7.Multiline string.dart
@@ -0,0 +1,24 @@
+void main() {
+// Multi Line Using Single Quotes
+  String multiLineText = '''
+1
+2
+3
+''';
+
+// Multi Line Using Double Quotes
+  String otherMultiLineText = """
+1
+2
+3.
+""";
+
+// Printing Information
+  print("Multiline text is $multiLineText");
+  print("Other multiline text is $otherMultiLineText");
+}
+
+
+
+// \n	New Line
+// \t	Tab
