b56a845aea52857b8fb305fe20b5a63cfebbcf9f
voidrlm <varjun609@gmail.com>
2023-02-16 20:21:52 +0530

    Read csv file

---
+++ 10.File Handling/2.read csv.dart
@@ -0,0 +1,16 @@
+// dart program to read from csv file
+import 'dart:io';
+
+void main() {
+  // open file
+  File file = File('test.csv');
+  // read file
+  String contents = file.readAsStringSync();
+  // split file using new line
+  List<String> lines = contents.split('\n');
+  // print file
+  print('---------------------');
+  for (var line in lines) {
+    print(line);
+  }
+}
