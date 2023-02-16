cc7f56f78e018a57d51485cf843769dda910c04a
voidrlm <varjun609@gmail.com>
2023-02-16 10:25:30 +0530

    Variables

---
+++ 1.Basics/3.variables.dart
@@ -0,0 +1,6 @@
+// String: For storing text value. E.g. “John” [Must be in quotes]
+// int: For storing integer value. E.g. 10, -10, 8555 [Decimal is not included]
+// double: For storing floating point values. E.g. 10.0, -10.2, 85.698 [Decimal is included]
+// num: For storing any type of number. E.g. 10, 20.2, -20 [both int and double]
+// bool: For storing true or false. E.g. true, false [Only stores true or false values]
+// var: For storing any value. E.g. ‘Bimal’, 12, ‘z’, true
