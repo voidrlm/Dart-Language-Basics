2433578d54c704e9de62fcac108403d26e572ad1
voidrlm <varjun609@gmail.com>
2023-02-16 20:21:41 +0530

    Read a file

---
+++ 10.File Handling/1.read file.dart
@@ -0,0 +1,19 @@
+// dart program to read from file
+import 'dart:io';
+
+void main() {
+  // creating file object
+  File file = File('test.txt');
+  // read file
+  String contents = file.readAsStringSync();
+  // print file
+  print(contents);
+   // get file location
+  print('File path: ${file.path}');
+  // get absolute path
+  print('File absolute path: ${file.absolute.path}');
+  // get file size
+  print('File size: ${file.lengthSync()} bytes');
+  // get last modified time
+  print('Last modified: ${file.lastModifiedSync()}');
+}
