b3f118716600e51a75461224aac49617b10c0409
voidrlm <varjun609@gmail.com>
2023-02-16 10:32:23 +0530

    Relational operators

---
+++ 1.Basics/15.relational operators.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+  //printing info
+  print(num1 == num2);
+  print(num1 < num2);
+  print(num1 > num2);
+  print(num1 <= num2);
+  print(num1 >= num2);
+}
