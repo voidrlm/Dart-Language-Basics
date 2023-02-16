0e85d819da2f5cf6df2c8d7e25d265e5783fedc4
voidrlm <varjun609@gmail.com>
2023-02-16 10:35:45 +0530

    Switch conditions

---
+++ 2.Condtions/2.Switch.dart
@@ -0,0 +1,29 @@
+void main() {
+  var number = 0;
+  switch (number) {
+    case 1:
+      print("sun");
+      break;
+    case 2:
+      print("mon");
+      break;
+    case 3:
+      print("tue");
+      break;
+    case 4:
+      print("wed");
+      break;
+    case 5:
+      print("thur");
+      break;
+    case 6:
+      print("fri");
+      break;
+    case 7:
+      print("sat");
+      break;
+    default:
+      print("failed.");
+      break;
+  }
+}
