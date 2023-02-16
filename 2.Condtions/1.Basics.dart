ee95c9f045bc6cf04d25a24ad96dcf2bd5ba386c
voidrlm <varjun609@gmail.com>
2023-02-16 10:35:28 +0530

    Basics of conditions

---
+++ 2.Condtions/1.Basics.dart
@@ -0,0 +1,18 @@
+void main() {
+  int num1 = 10;
+  int num2 = 0;
+
+  //
+  //normal if,else if,else
+  if (num2 > num2) {
+    print('num2 is greater');
+  } else if (num1 == num2) {
+    print('equal');
+  } else {
+    print('num2 is smaller');
+  }
+  //
+  // ternary
+  var check = (num2 > num2) ? 'num2 is greater' : 'else';
+  print(check);
+}
